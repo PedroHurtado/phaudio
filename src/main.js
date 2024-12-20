03d154f35f4a3cc102322154d81c3d5823022fb1
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-20 11:32:00 +0100

    change rollup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import {init, config} from '@audiorecorder/client'
 
-config.silero.url = "/test/worker_silero/index.js"
-config.url_worker_audio = "/test/worker_audio/index.js"
+config.silero.url = "/packages/worker_silero/dist/index.js"
+config.url_worker_audio = "/packages/worker_audio/dist/index.js"
 config.url_server = 'http://localhost:3000'
 
 init();
