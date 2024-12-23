3b7a0a4dbb84dab3825b9f30d9f806906830585e
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-23 17:03:15 +0100

    add reolver to common.js

--- packages/common/index.js
+++ packages/common/index.js
@@ -5,4 +5,5 @@ export * from './src/wavfile.js'
 export * from './src/emiter.js'
 export * from './src/idbkeyval.js'
 export * from './src/globalhandler.js'
+export * from './src/httpresponse/index.js'
 export default wavfile
