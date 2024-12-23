c6fe41ea78ace0796c5cfcb35f3144ac01f56fea
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-23 20:04:04 +0100

    add global handler exception in init

--- packages/client/src/main.js
+++ packages/client/src/main.js
@@ -3,7 +3,7 @@ import { getSession } from "./getsession.js";
 import { login } from "./login.js";
 import { getTimeServer } from "./timeserver.js";
 import { config } from "./config.js";
-import {add, globalHandler, remove} from '@audiorecorder/common'
+import { add, globalHandler, remove } from '@audiorecorder/common'
 
 config.options = {
   positiveSpeechThreshold: 0.5,
@@ -15,30 +15,37 @@ config.options = {
   submitUserSpeechOnPause: false,
 };
 async function init() {
-  const dispose = globalHandler(window);
-  const session = getSession(location.href)
-  const diff = await getTimeServer()  
-  const user = await login(session) 
+  try {
+    const session = getSession(location.href)
+    const diff = await getTimeServer()
+    const user = await login(session)
 
-  const audioRecorder = await AudioRecorder.new(
-    session,
-    diff
-  );
-  const localStream = await navigator.mediaDevices.getUserMedia({
-    audio: {
-      channelCount: 1,
-      echoCancellation: true,
-      autoGainControl: true,
-      noiseSuppression: true,
-    },
-    video: false,
-  });  
-  await audioRecorder.start(localStream); 
-  add("user", user);
-  return ()=>{
-    remove("user");
-    dispose();    
+    const audioRecorder = await AudioRecorder.new(
+      session,
+      diff
+    );
+    const localStream = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        channelCount: 1,
+        echoCancellation: true,
+        autoGainControl: true,
+        noiseSuppression: true,
+      },
+      video: false,
+    });
+    await audioRecorder.start(localStream);
+    await add("user", user);
+
+    const dispose = globalHandler(window);
+
+    return async () => {
+      await remove("user");
+      dispose();
+    }
+  } catch (e) {
+    console.error(e)
   }
+
 }
-export {init,config}
+export { init, config }
 
