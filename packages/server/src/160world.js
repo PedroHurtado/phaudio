646d69e58940799d11064df678ea8db1b3dac8e4
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-15 18:05:46 +0100

    server credentials

--- packages/server/src/160world.js
+++ packages/server/src/160world.js
@@ -1,13 +1,21 @@
 import { resolve } from "@audiorecorder/common";
 const URL = process.env.URL_SERVER
-const credentials = `${btoa(process.env.API_USER)}:${btoa(process.env.API_TOKEN)}`
+const credentials = btoa(`${process.env.API_USER}:${process.env.API_TOKEN}`)
 function getUrl(session,path){
     const {peer_id,room_id,token} = session;
     return `${URL}/${peer_id}/${room_id}/${token}/${path}`
 }
 
+async function processResponse(response){
+    const {status} = response
+    if(status === 200){
+        return true
+    }
+    return await resolve(response);
+}
+
 const headers={
-        autorization:`basic ${credentials}`
+    Authorization:`Basic ${credentials}`
 }
 export async function validate(session){    
     const url = getUrl(session,'validate')
@@ -15,7 +23,8 @@ export async function validate(session){
         method:'GET',
         headers:headers,        
     })
-    return resolve(response);
+    return await processResponse(response)
+    
 }
 export async function transcript({sessionRoom}, data) {
     const url = getUrl(sessionRoom, 'transcript')
@@ -23,6 +32,6 @@ export async function transcript({sessionRoom}, data) {
         method:'POST',
         headers:{...headers,... {'content-type':'aplicación/json'}},
         body:JSON.stringify(data)
-    })    
-    return resolve(response);
+    })       
+    return await processResponse(response)
 }
